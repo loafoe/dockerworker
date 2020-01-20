ceaa7b7f26fd1a441eb166679bf0b7cb9c4cb530
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-01-20 14:12:33 +0100

    Upgrade to Go 1.13 and modules

---
+++ go/go.mod
@@ -0,0 +1,8 @@
+module hello
+
+go 1.13
+
+require (
+	github.com/iron-io/iron_go v0.0.0-20180607082653-56e218bf01ba
+	github.com/jeffh/go.bdd v0.0.0-20120717032931-88f798ee0c74 // indirect
+)
--- go/hello.go
+++ go/hello.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/iron-io/iron_go/worker"
 )
@@ -14,6 +16,11 @@ func main() {
 	worker.ParseFlags()
 	p := &Person{}
 	worker.PayloadFromJSON(p)
+	payloadFile := os.Getenv("PAYLOAD_FILE")
+	fmt.Println("PAYLOAD_FILE is", payloadFile)
+	fmt.Println("Payload:")
+	payload, _ := ioutil.ReadFile(payloadFile)
+	fmt.Println(string(payload))
 	fmt.Println("Hello go! My name is", p.Name)
 	fmt.Println("Task id:", worker.TaskId)
 }
